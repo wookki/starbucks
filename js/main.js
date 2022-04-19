6faa01dfe77679472e0db30ede869e006a0ed60b
wookki <bwlimtony@gmail.com>
2022-04-19 21:09:30 +0900

    뱃지 이미지 수정

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function(){
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
   console.log(window.scrollY);
@@ -25,14 +26,20 @@ window.addEventListener('scroll', _.throttle(function(){
       opacity: 0,
       display: 'none'
     });
-  }
-  else {
+    gsap.to(toTopEl, .2, {
+      x: 0
+  });
+} else {
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
  }, 300));
+
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -40,6 +47,13 @@ fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1,{
     delay: (index + 1) * .7,
     opacity: 1
+  });
+});
+
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
   });
 });
 
@@ -66,6 +80,21 @@ new Swiper('.promotion .swiper', {
     nextEl: '.promotion .swiper-next'
   }
 });
+new Swiper('.awards .swiper', { //swiper-container 아님
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+
+
+
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion')
 let isHidePromotion = false;
@@ -103,7 +132,7 @@ floatingObject('.floating3', 1.5, 20);
 
 
 
-const spyEls = document.querySelectorAll('section.scroll-spy')
+const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({
@@ -111,5 +140,8 @@ spyEls.forEach(function (spyEl) {
       triggerHook: .8
     })
     .setClassToggle(spyEl, 'show')
-    .addTo(new ScrollMagic.controller());
-});
+    .addTo(new ScrollMagic.Controller()) //대문자 주의
+})
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2021
